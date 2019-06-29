865a2a7981b7e14f4c22a1afb1a61b35cb3cfd35
Tran Van Tai <tooyoutooyou@gmail.com>
2019-06-29 19:17:25 +0700

    add code

--- src/main/webapp/resources/js/account.js
+++ src/main/webapp/resources/js/account.js
@@ -1,66 +1,89 @@
-$(function() {
-	$('#frmLogin').submit(function() {
-		console.log($(this).serialize());
-		$.ajax({
-			url : $(this).attr('action'),
-			type : $(this).attr('method'),
-			data : $(this).serialize(),
-			success : function(res) {
-				if (res) {
-					// Trả về true => load lại trang
-					location.reload();
-				} else {
-					swal({
-						title : "Thông báo!",
-						text : "Sai tên đăng nhập hoặc mật khẩu!",
-						icon : "error",
-						button : "Đóng",
-					});
-				}
-			},
-			error : function(err) {
-				console.log(err);
-			}
-		});
+$(function () {
+  $('#frmLogin').submit(function () {
+    console.log($(this).serialize());
+    $.ajax({
+      url: $(this).attr('action'),
+      type: $(this).attr('method'),
+      data: $(this).serialize(),
+      success: function (res) {
+        if (res) {
+          // Trả về true => load lại trang
+          location.reload();
+        } else {
+          swal({
+            title: "Thông báo!",
+            text: "Sai tên đăng nhập hoặc mật khẩu!",
+            icon: "error",
+            button: "Đóng",
+          });
+        }
+      },
+      error: function (err) {
+        console.log(err);
+      }
+    });
 
-		return false;
-	});
+    return false;
+  });
 });
 
 // Khi bàn phím được nhấn và thả ra thì sẽ chạy phương thức này
 $("#frmRegister").validate({
-	rules : {
-		fullname : {
-			required : true,
-			minlength : 4
-		},
-		email : {
-			required : true,
-			email : true
-		},
-		password : {
-			required : true,
-			minlength : 6
-		},
-		confirm : {
-			equalTo : "#password"
-		}
-	},
-	messages : {
-		fullname : {
-			required : "Vui lòng nhập họ tên!",
-			minlength : "Họ tên ít nhất 4 ký tự!"
-		},
-		email : {
-			required : "Vui lòng nhập email!",
-			email : "Email không đúng định dạng!"
-		},
-		password : {
-			required : 'Vui lòng nhập mật khẩu',
-			minlength : 'Vui lòng nhập ít nhất 6 kí tự'
-		},
-		confirm : {
-			equalTo : 'Mật khẩu nhập không khớp!'
-		}
-	}
+  rules: {
+    fullname: {
+      required: true,
+      minlength: 4
+    },
+    email: {
+      required: true,
+      email: true
+    },
+    password: {
+      required: true,
+      minlength: 6
+    },
+    confirm: {
+      equalTo: "#password"
+    }
+  },
+  messages: {
+    fullname: {
+      required: "Vui lòng nhập họ tên!",
+      minlength: "Họ tên ít nhất 4 ký tự!"
+    },
+    email: {
+      required: "Vui lòng nhập email!",
+      email: "Email không đúng định dạng!"
+    },
+    password: {
+      required: 'Vui lòng nhập mật khẩu',
+      minlength: 'Vui lòng nhập ít nhất 6 kí tự'
+    },
+    confirm: {
+      equalTo: 'Mật khẩu nhập không khớp!'
+    }
+  },
+  submitHandler: function (form, event) {
+    console.log(form);
+    var obj = {
+      fullname: $('#fullname').val(),
+      email: $('#email').val(),
+      password: $('password').val(),
+      confirm: $('#confirm').val(),
+      roleId:$('#roleId').val()
+    }
+    $.ajax({
+      url: $(form).attr('action'),
+      type: $(form).attr('method'),
+      data: JSON.stringify(obj),
+      contentType: 'application/json',
+      dataType: 'json',
+      success: function (data) {
+        console.log(data);
+      },
+      error: function (err) {
+        console.log(err);
+      }
+    })
+  }
 });
